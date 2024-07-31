2b8c34694ab9d4e5b4c88bcbf4c9817317aba7ba
Praveen123445 <Pravin324@gmail.com>
2024-07-31 12:38:14 +0530

    all setup config

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,5 @@
-import { Promise } from "mongoose"
-
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
